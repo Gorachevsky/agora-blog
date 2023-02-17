71e886a8ba389e6ba4e0af097cb0b182b0e2cf9b
Gorachevsky <anton.gorachevskiy@gmail.com>
2023-02-17 20:26:01 +0000

    Added drafts feature & new Header buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,40 @@
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header: React.FC = () => {
-  return (
-    <div className="h-12 lg:h-16 flex justify-center items-center">
-      TODO HEADER
+  const router = useRouter();
+  const isActive: (pathname: string) => boolean = (pathname) =>
+    router.pathname === pathname;
+
+  let left = (
+    <div className="left">
+      <Link href="/" legacyBehavior>
+        <a className="bold" data-active={isActive("/")}>
+          Feed
+        </a>
+      </Link>
+      <Link href="/drafts" legacyBehavior>
+        <a data-active={isActive("/drafts")}>My drafts</a>
+      </Link>
     </div>
+  );
+
+  let right = (
+    <div className="right">
+      <Link href="/create" legacyBehavior>
+        <button>
+          <a>New post</a>
+        </button>
+      </Link>
+    </div>
+  );
+
+  return (
+    <nav>
+      {left}
+      {right}
+    </nav>
   );
 };
 
