07706ddc1d77de8eaf71dcf169bdc6a7de4ef022
Gorachevsky <anton.gorachevskiy@gmail.com>
2023-02-18 13:36:49 +0000

    Fix new Post - createdAt v3

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -54,7 +54,7 @@ const Post: React.FC<PostProps> = (props) => {
           <h2 className="text-4xl font-bold my-4">{title}</h2>
           <p className="ml-2 italic text-gray-400">
             By {props?.author?.name || "Unknown author"} |{" "}
-            {props?.createdAt?.substring(0, 10) || ""}
+            {props.createdAt.substring(0, 10) || ""}
           </p>
           <p className="ml-2 my-4 whitespace-normal">{props.content}</p>
           {!props.published && (
