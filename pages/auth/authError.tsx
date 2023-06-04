ab5e94712d909d965a78ed4088d6efd2c496cb04
Gorachevsky <anton.gorachevskiy@gmail.com>
2023-06-04 02:23:19 +0200

    Auth layout fix

--- pages/auth/authError.tsx
+++ pages/auth/authError.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import Layout from "../../components/layout";
 
 const Draft: React.FC = () => {
   return (
-    <Layout>
       <div className="flex flex-col justify-center items-center">
         Auth Error :(
       </div>
-    </Layout>
   );
 };
 
